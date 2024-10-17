bf0d310f819f54607ba7b310d8197c6ef9a31e7f
Jay <jaygondaliya2309@gmail.com>
2024-10-17 07:35:53 +0530

    Added Insertion Sort and Merge Sort in Java

---
+++ Java/Algorithms/Sorting/Insertion_Sort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Insertion_Sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        insertionSort(arr);
+        System.out.println("The sorted array is:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
