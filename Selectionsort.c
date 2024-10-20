326e7ceeaa0fd02bf8d398656d0e613fea82b3fa
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-10-20 12:16:05 +0530

    Create Selectionsort.c

---
+++ Selectionsort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, minIdx, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        minIdx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        temp = arr[minIdx];
+        arr[minIdx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    // Prompt the user for the number of elements
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n]; // Declare an array of size n
+
+    // Read the elements from the user
+    printf("Enter %d numbers: \n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array: \n");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
