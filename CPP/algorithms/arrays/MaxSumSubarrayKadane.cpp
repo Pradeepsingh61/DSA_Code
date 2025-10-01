11d22da60fab9304a377bb5a42a167b5d7f83b02
Kinshu-Learner <kinshurajgupta1@gmail.com>
2025-10-01 22:31:44 +0530

    fixed standard template issues

--- CPP/algorithms/arrays/MaxSumSubarrayKadane.cpp
+++ CPP/algorithms/arrays/MaxSumSubarrayKadane.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
 #include<vector>
 #include<climits>
-
-using namespace std;
+#include<algorithm>
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray(std::vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
         int maxSum = INT_MIN; // Stores the maximum sum found so far
         int currSum = 0;      // Stores the sum of the current subarray
-        for(int i = 0; i < nums.size(); i++){
+        for(size_t i = 0; i < nums.size(); i++){
             currSum += nums[i];
-            maxSum = max(currSum, maxSum);
+            maxSum = std::max(currSum, maxSum);
             
             // If the current sum is negative, it won't contribute to a larger sum,
             // so we reset it to 0 to start a new subarray from the next element.
@@ -23,3 +25,14 @@ public:
         return maxSum;
     }
 };
+
+int main() {
+    Solution sol;
+    std::vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    
+    int result = sol.maxSubArray(nums);
+    
+    std::cout << "The maximum subarray sum is: " << result << std::endl;
+    
+    return 0;
+}
