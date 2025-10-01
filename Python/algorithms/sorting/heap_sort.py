f9fed5d52b34d62045f647c0c02b932c797f1190
Tanishka Pal <tanishka.ppal@gmail.com>
2025-10-01 18:59:12 +0530

    Create heap_sort.py

---
+++ Python/algorithms/sorting/heap_sort.py
@@ -0,0 +1,32 @@
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[l] > arr[largest]:
+        largest = l
+    if r < n and arr[r] > arr[largest]:
+        largest = r
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heapSort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
+
+# Taking user input
+input_str = input("Enter numbers separated by space: ")
+arr = list(map(int, input_str.split()))
+
+heapSort(arr)
+
+print("Sorted array is")
+for i in arr:
+    print(i, end=' ')
