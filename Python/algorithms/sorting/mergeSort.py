c183f637c12cef3ee184c640dc8f533e75b89470
Vijaya Mishra <134155725+Vijayaa21@users.noreply.github.com>
2025-10-03 19:29:51 +0530

    Update mergeSort.py

--- Python/algorithms/sorting/mergeSort.py
+++ Python/algorithms/sorting/mergeSort.py
@@ -28,3 +28,45 @@ Stability:
     Merge Sort is a **stable sorting algorithm**, meaning it maintains the 
     relative order of equal elements.
 """
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  
+        L = arr[:mid]        
+        R = arr[mid:]
+
+        # Recursive calls to sort both halves
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+
+        # Merge the two halves
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:   
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Copy any remaining elements of L
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        # Copy any remaining elements of R
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+
+if __name__ == "__main__":
+    arr = [38, 27, 43, 3, 9, 82, 10]
+    print("Original array:", arr)
+    merge_sort(arr)
+    print("Sorted array:", arr)
+# Sorted array: [3, 9, 10, 27, 38, 43, 82]
