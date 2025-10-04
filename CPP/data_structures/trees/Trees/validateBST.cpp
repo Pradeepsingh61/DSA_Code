3dd956bef68cd3f2e20ac96bdb683e83dd7dbbb0
Arihant Jain <78062831+arihant-jain8@users.noreply.github.com>
2025-10-05 00:49:51 +0530

    Update validateBST.cpp
    added the description for TreeNode

--- CPP/data_structures/trees/Trees/validateBST.cpp
+++ CPP/data_structures/trees/Trees/validateBST.cpp
@@ -13,17 +13,17 @@
  * The space complexity is O(h), where h is the height of the tree, due to the recursion stack.
  */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
 class Solution {
 public:
     TreeNode *prev = NULL;
