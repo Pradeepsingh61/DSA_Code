7ab0de98cba729b7be441d4ab918b60ecb4c6ffa
Revant <revant0501@gmail.com>
2025-10-04 18:32:26 +0530

    Rename public class to BFSShortestPath to fix CI compilation

--- Java/algorithms/graph_algorithms/BFSShortestPath.java
+++ Java/algorithms/graph_algorithms/BFSShortestPath.java
@@ -32,7 +32,7 @@
 
 import java.util.*;
 
-public class BFSTraversal {
+public class BFSShortestPath {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // Input vertices and edges
