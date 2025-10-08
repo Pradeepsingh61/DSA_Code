625a53c6c9476c1f10077353a8c7b26fbec148d2
priyashuu <priyaaashu4@gmail.com>
2025-10-08 15:26:42 +0530

    Enhance documentation for DeleteNodeInLinkedList algorithm with detailed description and complexity analysis

--- Java/data_structures/linked_lists/DeleteNodeInLinkedList.java
+++ Java/data_structures/linked_lists/DeleteNodeInLinkedList.java
@@ -1,3 +1,28 @@
+/*
+ * Algorithm Name:Delete Node in a Linked List .
+ * Programming Language: Java
+ * Category: Linked List  
+ * Difficulty Level: Medium
+ *
+ * Author: Priya Rani 
+ *
+ * Algorithm Description:
+ * There is a singly-linked list head and we want to delete a node node in it.
+
+You are given the node to be deleted node. You will not be given access to the first node of head.
+
+All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+
+Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+
+The value of the given node should not exist in the linked list.
+The number of nodes in the linked list should decrease by one.
+All the values before node should be in the same order.
+All the values after node should be in the same order.
+ *
+ * Time Complexity: O(1) -No traversal or loops are used, so the time complexity is O(1).
+ * Space Complexity: O(1) - The algorithm uses only a constant amount of extra space (a few variables, no additional data structures).
+ */
 class ListNode {
     int val;
     ListNode next;
