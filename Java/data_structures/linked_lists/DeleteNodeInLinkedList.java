35281c27358d442296712eed810a721dfcf8617c
Pradeep <144537409+Pradeepsingh61@users.noreply.github.com>
2025-10-08 21:38:55 +0530

    Update and rename DeleteNodeInLinkedListPriya.java to DeleteNodeInLinkedList.java

--- Java/data_structures/linked_lists/DeleteNodeInLinkedList.java
+++ Java/data_structures/linked_lists/DeleteNodeInLinkedList.java
@@ -31,7 +31,7 @@ class ListNode {
     }
 }
 
-public class DeleteNodeInLinkedListPriya {
+public class DeleteNodeInLinkedList {
     public void deleteNode(ListNode node) {
         node.val = node.next.val; 
         node.next = node.next.next;
