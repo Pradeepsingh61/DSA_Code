2f808ed905499944a9dafe8ca0189cbf1b861b80
priyashuu <priyaaashu4@gmail.com>
2025-10-08 14:48:34 +0530

    Refactor SpiralMatrix to remove commented-out code and improve readability

--- Java/algorithms/arrays/SpiralMatrix.java
+++ Java/algorithms/arrays/SpiralMatrix.java
@@ -10,28 +10,20 @@ public class SpiralMatrix {
         int right = matrix[0].length - 1;
 
         while (top <= bottom && left <= right) {
-            
-            // Traverse left → right
             for (int i = left; i <= right; i++) {
                 result.add(matrix[top][i]);
             }
             top++;
-
-            // Traverse top → bottom
             for (int i = top; i <= bottom; i++) {
                 result.add(matrix[i][right]);
             }
             right--;
-
-            // Traverse right → left
             if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
                     result.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
-
-            // Traverse bottom → top
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     result.add(matrix[i][left]);
@@ -39,7 +31,16 @@ public class SpiralMatrix {
                 left++;
             }
         }
-
         return result;
+    }
+
+    public static void main(String[] args) {
+        SpiralMatrix sm = new SpiralMatrix();
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println(sm.spiralOrder(matrix));
     }
 }
