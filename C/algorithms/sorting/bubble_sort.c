9807c13d5d23ba4fe5ae00d887e8ba0ad1ffbf97
kavvyaaa-a <kavyaraj26oct@gmail.com>
2025-10-28 22:32:10 +0530

    changes

--- C/algorithms/sorting/bubble_sort.c
+++ C/algorithms/sorting/bubble_sort.c
@@ -1,79 +1,72 @@
-/*
- * Algorithm: [Bubble Sort]
- * Description: [It starts from the beginning of the list and compares each
- *               pair of adjacent elements, and swap them if they are in the
- *               wrong order]
- * Time Complexity :
- *     Best Case : O(n)  // when given array is already sorted.
- *     Average Case : O(n^2) //when given array is in random order
- *     Worst Case : O(n^2) // when given array is in reverse order
- * Space Complexity:
- *   Worst : 0(1)
- * Author: [tanshen-kun]
- */
+// Bubble Sort Implementation in C
+// Hacktoberfest 2025 Contribution
 
 #include <stdio.h>
 
-/*
- * bubble_sort : it takse the array and its size then it starts to travel array
- * while comparing successive pairs of element until the travel reach the last
- * element
- * @param arr: Array to process
- * @param size: Size of the array
- * @return: None
- */
-
-void bubble_sort(int arr[], int size) {
-  for (int i = 0; i < size - 1; i++) {
-    for (int j = 0; j < size - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        int temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-}
-
-/*
- * printArray: Prints a given array
- *
- *@param arr: Array to print
- *@param size: Size of the array
- */
-void printArray(int arr[], int size) {
-  for (int i = 0; i < size; i++) {
-    printf("%d ", arr[i]);
-  }
-  printf("\n");
-}
-
-void test_algorithm() {
-  printf("Testing Algorithm...\n");
-
-  // Test Case 1 : Already Sorted
-  int test_arr1[] = {1, 2, 3, 4, 5};
-  int size1 = sizeof(test_arr1) / sizeof(test_arr1[0]);
-  printf("Test 1 : ");
-  bubble_sort(test_arr1, size1);
-  printArray(test_arr1, size1);
-
-  // Test Case 2 : Random order
-  int test_arr2[] = {3, 1, 5, 2, 4};
-  int size2 = sizeof(test_arr2) / sizeof(test_arr2[0]);
-  printf("Test 2 : ");
-  bubble_sort(test_arr2, size2);
-  printArray(test_arr2, size2);
-
-  // Test Case 3 : Reverse Order
-  int test_arr3[] = {5, 4, 3, 2, 1};
-  int size3 = sizeof(test_arr3) / sizeof(test_arr3[0]);
-  printf("Test 3 : ");
-  bubble_sort(test_arr3, size3);
-  printArray(test_arr3, size3);
-}
+// Function prototypes
+void inputArray(int arr[], int size);
+void displayArray(int arr[], int size);
+void bubbleSort(int arr[], int size);
 
 int main() {
-  test_algorithm();
-  return 0;
+    int size;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    // Get input from user
+    inputArray(arr, size);
+
+    printf("\nOriginal array:\n");
+    displayArray(arr, size);
+
+    // Sort array using Bubble Sort
+    bubbleSort(arr, size);
+
+    printf("\nSorted array in ascending order:\n");
+    displayArray(arr, size);
+
+    return 0;
+}
+
+// Reads 'size' number of elements into the array
+void inputArray(int arr[], int size) {
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
+// Displays the contents of the array
+void displayArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Performs Bubble Sort on the array
+void bubbleSort(int arr[], int size) {
+    int temp;
+    int swapped;
+
+    for (int i = 0; i < size - 1; i++) {
+        swapped = 0;
+
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        // Optimization: stop if array is already sorted
+        if (!swapped)
+            break;
+    }
 }
